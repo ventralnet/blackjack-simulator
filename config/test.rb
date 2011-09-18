ea6896db5b323815e6201bc540cca52a5e0f5440
Matthew Kirkley <mkirkley@texeltek.com>
2011-09-18 16:11:11 -0400

    strategy util

--- config/test.rb
+++ config/test.rb
@@ -1,32 +1,11 @@
 require 'yaml'
 
 yaml = begin
-								 YAML.load(File.open("strategy.yaml"))
+								 YAML.load(File.open("strategy_pairs.yaml"))
 				 rescue ArgumentError => e
 								  puts "Could not parse YAML: #{e.message}"
 				 end
 
 yaml.each_key {|key|
-  puts "#{key} and values are #{yaml[key][7]}" 
+  puts "#{key} and values are #{yaml[key][:ace]}" 
 }
-
-puts
-puts
-puts
-puts
-
-#class Test
-
-#	def initialize
-#  @my_hash = {:a9 => {:5 => 'stay',:8 => 'hit'}}
-#	 @b = "hello"
-#	end
-
-#end
-
-#puts Test.new.to_yaml
-
-a = { :a => {:b => :test, :c => :ace}}
-
-puts a.to_yaml
-
